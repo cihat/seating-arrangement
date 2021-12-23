ba9e09ac87b821ac204b00df0c6960eb0fb37bb6
cihat <cihat02020202@gmail.com>
2021-12-24 02:01:26 +0300

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
+
+// TODOS: Login with just school number
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Welcome to Library" })
+})
 
-module.exports = router;
+router.post("/", (req, res, next) => {
+  let { schoolId } = req.body
+  schoolId = Number(schoolId)
+
+  if (typeof schoolId == "number")
+    res.redirect(`/dashboard/?schoolId=${Number(schoolId)}`)
+  else res.redirect("/")
+})
+
+router.get("/dashboard", (req, res, next) => {
+  let { schoolId } = req.query
+  schoolId = Number(schoolId)
+  if (!schoolId || typeof schoolId !== "number") {
+    res.redirect("/")
+  }
+  res.render("dashboard", { schoolId })
+})
+
+module.exports = router
