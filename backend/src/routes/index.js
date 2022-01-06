4f2fc92f09bf594f4ae1502861471ad57eb53cca
cihat <cihat02020202@gmail.com>
2022-01-06 15:41:29 +0300

    Add basit route in index

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const Library = require('../models/library')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' })
+router.get('/', async (req, res) => {
+  await Library.find({ _id: '61d6871991e8443c13878546' })
+    .then(lib => res.send(lib[0]))
+    .catch(error => res.send(error))
 })
 
 router.get('/ping', (req, res) => {
