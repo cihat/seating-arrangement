52b1709427a8d0c13a4f3777c314524f3749f1e3
cihat <cihat02020202@gmail.com>
2022-01-06 07:54:24 +0300

    Add config for mongoDB connections

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-const connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost'
+const connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017/seating-arrangement'
 
 mongoose.set('debug', true)
 
@@ -10,7 +11,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('Database connection established.'))
+  .then(() => console.log('Database connection established.🚀'))
   .catch(console.log)
 
 module.exports = mongoose.connection
