c34cdb3f2b68e05f0499687a143f86873c6ab161
cihat <cihat02020202@gmail.com>
2022-01-06 15:42:05 +0300

    Update students and libraries routes

--- backend/src/routes/libraries.js
+++ backend/src/routes/libraries.js
@@ -9,41 +9,34 @@ const createEmptySeats = require('../config/createEmptySeats')
 const router = Router()
 
 router.get('/', async (req, res) => {
-  res.send(await Library.find({}))
+  await Library.find({ _id: '61d6e2ca7bea1b6203056ee6' })
+    .then(lib => res.send(lib))
+    .catch(error => res.send(error))
 })
 
 router.get('/create-library/:libraryId', async (req, res, next) => {
   // const { libraryId, totalCapacity, floorsCapacity, numberOfTables, seatsDetails, intensity } = req.query
   const { libraryId } = req.params
-  // console.log(libraryId)
+
   try {
     const sameLibraryId = await Library.find({ libraryId })
-    // console.log('sameLibraryId: ', sameLibraryId)
     if (sameLibraryId.length > 1) {
       res.send({
         error: 'Library ID is already used.',
       })
     }
 
-    console.log('createEmptySeats: ', createEmptySeats(100))
-
     const library = await Library.create({
       libraryId,
-      totalCapacity: 2000,
-      floorsCapacity: [100, 200, 300, 400, 10000],
-      seatsDetails: createEmptySeats(200),
+      libraryName: 'Firat University Library',
+      totalCapacity: 800,
+      floorsCapacity: [200, 150, 300, 150],
+      seatsDetails: createEmptySeats(800),
       intensity: 'low',
     })
-
-    // library.seatsDetails = { library_dummy_data }
-    console.log('library', library)
-
-    // console.log(library)
     res.send(library)
   } catch (error) {
-    res.send({
-      error: error.message,
-    })
+    res.redirect('/libraries')
   }
 })
 
--- backend/src/routes/students.js
+++ backend/src/routes/students.js
@@ -11,24 +11,69 @@ router.get('/', async (req, res) => {
 
 router.get('/sign-up/:studentId', async (req, res, next) => {
   const { studentId } = req.params
-  console.log(studentId)
+
   try {
     const sameStudentId = await Student.find({ studentId })
-    console.log('sameStudentId: ', sameStudentId)
     if (sameStudentId.length > 1) {
       res.send({
         error: 'Student ID is already used.',
       })
     }
-    const student = await Student.create({ studentId })
+    const student = await Student.create({
+      studentId,
+      libraryId: '61d6e2ca7bea1b6203056ee6',
+      breaksUsed: {
+        short: {
+          counter: 0,
+          dates: [],
+        },
+        long: {
+          counter: 0,
+          sessionLimit: 2,
+          dates: [],
+        },
+      },
+      selectedSeatNumber: 1,
+      inputDate: new Date(),
+    })
 
-    console.log(student)
     res.send(student)
   } catch (error) {
-    res.send({
-      error: error.message,
-    })
+    res.redirect('/students')
   }
 })
+
+router.get('/:studentId/select-empty-seat/:selectedSeatNumber', (req, res, next) => {
+  const { studentId, selectedSeatNumber } = req.params
+
+  Student.findById(studentId, (err, student) => {
+    student.selectedSeatNumber = selectedSeatNumber
+    const selectedSeat = student.libraryId.seatsDetails[selectedSeatNumber - 1]
+    selectedSeat.status = true
+    selectedSeat.sittingStudentId = studentId
+
+    student.save()
+    res.send(student)
+  })
+})
+
+//! Authentication
+// TODO: Create new student sign-up --> router.post("/sign-up")
+// TODO: Login student sign-in --> router.post("/sign-in")
+// TODO: Create new student sign-out --> router.post("/sign-out")
+
+//! Actions
+// TODO: Selecting a empty place for the logged in student --> router.post("/select-empty-seat")
+// TODO: May take a break for student entering the library --> router.post("/take-a-break")
+// TODO: May take a break for student leaving the break --> router.post("/leave-a-break")
+// TODO: May take a break for student leaving the library --> router.post("/leave-a-break")
+
+//! Getters
+// TODO: Get all students --> router.get("/")
+// ToDo: Get all students who are in the break --> router.get("/in-break")
+// TODO: Get all students who are in the library --> router.get("/in-library")
+// TODO: Get all seatsDetails --> router.get("/seats-details")
+
+//! Coming Soon
 
 module.exports = router
