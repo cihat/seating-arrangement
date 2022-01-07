ae59931694ba8f6c1044b259257d1c1f89b977d7
cihat <cihat02020202@gmail.com>
2022-01-07 20:29:31 +0300

    Update creat empty seats index number +1

--- backend/src/config/createEmptySeats.js
+++ backend/src/config/createEmptySeats.js
@@ -1,7 +1,7 @@
 const createEmptySeats = seats => {
   const emptySeats = new Array(seats).fill({})
   emptySeats.forEach((seat, index) => {
-    emptySeats[index] = {
+    emptySeats[index + 1] = {
       status: false,
       sittingStudentId: '',
       seatNumber: index + 1,
