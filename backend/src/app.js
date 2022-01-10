b32cc96bb09c2648cc507671afc23abee730b93a
cihat <cihat02020202@gmail.com>
2022-01-11 00:44:07 +0300

    Add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-  
+const cors = require('cors')
+
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const passport = require('passport')
@@ -50,6 +51,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
 
 app.use('/', indexRouter)
 app.use('/students', studentsRouter)
