c40441bb47e84d53d137de1da0b3855e007d3333
cihat <cihat02020202@gmail.com>
2022-01-13 03:51:28 +0300

    Add seats router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/home.vue'
+import Seats from '../views/seats.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/seats',
+    name: 'Seats',
+    component: Seats,
   },
   {
     path: '/about',
