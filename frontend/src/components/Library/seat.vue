0f02f44623be09a51adc1173b04018e159341de6
cihat <cihat02020202@gmail.com>
2022-01-13 03:50:30 +0300

    Add seats view and seat component

---
+++ frontend/src/components/Library/seat.vue
@@ -0,0 +1,54 @@
+<script>
+import { mapActions, mapState } from 'vuex'
+export default {
+  name: 'Seat',
+  data() {
+    return {
+      seats: [],
+    }
+  },
+  async created() {
+    await this.fetchLibrary()
+  },
+  computed: {
+    ...mapState({
+      libraries: 'libraries',
+    }),
+    json: json => {
+      console.log(json)
+      return json[0].seatsDetails
+    },
+  },
+  methods: {
+    ...mapActions(['fetchLibrary']),
+  },
+}
+</script>
+
+<template lang="pug">
+.seats_container
+  h1
+    | Seat Selection
+  .seats
+    ul
+      li(v-for="library in libraries" :key="library._id")
+          a(href="#") {{library}}
+
+  //- .libraries
+  //-   pre
+  //-     code
+  //-       | {{ JSON.stringify(libraries[0], null, 2) }}
+</template>
+
+<style lang="scss">
+.seats_container {
+  .seats {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 0 auto;
+    max-width: 800px;
+    padding: 0;
+  }
+}
+</style>
---
+++ frontend/src/views/seats.vue
@@ -0,0 +1,15 @@
+<script>
+import Seat from '../components/Library/seat.vue'
+export default {
+  name: 'Seats',
+  components: {
+    Seat,
+  },
+}
+</script>
+
+<template lang="pug">
+.seats
+  h1 This is an seats page.
+  Seat
+</template>
